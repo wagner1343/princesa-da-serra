a83925e19e15a4fb05e2c6d407890e1e3380f086
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2018-10-04 19:50:57 -0300

    Nao lembro o que fiz

--- src/princesadaserra/java/ui/view/trips/TripsView.java
+++ src/princesadaserra/java/ui/view/trips/TripsView.java
@@ -6,20 +6,5 @@ import java.util.Observable;
 import java.util.Observer;
 
 public class TripsView {
-	
-	
-	TripsViewModel viewModel;
-	
-	TextField t; 
-	
-	private void init(){
-		viewModel.recent().addObserver(new Observer() {
-			
-			@Override
-			public void update(Observable o, Object arg) {
-				
-				t.setText(arg.getClass().toString());
-			}
-		});
-	}
+
 }
