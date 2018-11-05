4d4c70b58aed23fb84c89e1e471cc432b838994a
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2018-11-05 17:13:22 -0200

    arrumar

--- tests/auth/TestAuthService.java
+++ tests/auth/TestAuthService.java
@@ -6,9 +6,6 @@ import princesadaserra.java.service.AuthService;
 public class TestAuthService {
     public static void main(String[] args) {
         AuthService authService = AuthService.getInstance();
-        User user = new User();
-        user.setLogin("wagner1343");
-
-        authService.register("test_user4", "13431343", "test_role");
+        authService.register("test_user6", "13431343", "test_rolse");
     }
 }
