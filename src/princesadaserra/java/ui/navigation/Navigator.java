09bcd4931a28f38ebee1801402cbb7d8b0649132
Wagner Silvestre Wuchryn Martins <wagner1343@outlook.com>
2018-11-09 15:41:21 -0200

    Preloader deletado, navigateTo refatorado

--- src/princesadaserra/java/ui/navigation/Navigator.java
+++ src/princesadaserra/java/ui/navigation/Navigator.java
@@ -8,40 +8,34 @@ import princesadaserra.java.ui.controller.ScenesTypes;
 import princesadaserra.java.util.context.ResourcesHolder;
 
 import java.io.IOException;
-import java.net.URL;
 
 public class Navigator {
     private Stage stage;
     private Scene rootScene;
     private FXMLLoader fxmlLoader;
-    private Preloader<URL, Parent> preloader;
 
     public Navigator(Stage stage) {
         this.stage = stage;
         fxmlLoader = new FXMLLoader();
-        preloader = new Preloader<>((url) -> {
-            try {
-                return FXMLLoader.load(url, ResourcesHolder.getResourceBundle());
-            } catch (IOException e) {
-                e.printStackTrace();
-                return null;
-            }
-        });
-
-        for (ScenesTypes scene : ScenesTypes.values())
-            preloader.preload(getClass().getResource(scene.getPath()));
     }
 
     public void navigateTo(ScenesTypes sceneType) {
         System.out.println("Navigator.navigateTo:" + "sceneType = [" + sceneType + "]");
-        Parent nextRoot = preloader.load(getClass().getResource(sceneType.getPath()));
+        Parent nextRoot;
+        try {
+            nextRoot = fxmlLoader.load(getClass().getResource(sceneType.getPath()), ResourcesHolder.getResourceBundle());
 
-        if (rootScene == null) {
-            rootScene = new Scene(nextRoot);
-            stage.setScene(rootScene);
-        } else
-            rootScene.setRoot(nextRoot);
+            if (rootScene == null) {
+                rootScene = new Scene(nextRoot);
+                stage.setScene(rootScene);
+            }
+            else rootScene.setRoot(nextRoot);
 
-        if (!stage.isShowing()) stage.show();
+            if (!stage.isShowing()) stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
